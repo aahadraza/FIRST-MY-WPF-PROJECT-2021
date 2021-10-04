a4d43b21733722a09183ad1753815764abac7ac2
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-10-04 04:39:48 -0700

    CLICK AND SHOW CONTENT

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CLICK_AND_SHOW_CONTENT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
